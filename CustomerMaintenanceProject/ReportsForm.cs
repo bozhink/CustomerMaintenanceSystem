c4b72d5c9a13694839a7dc0d071dc2cccc9356bc
Bozhin Karaivanov <bozhink@gmail.com>
2016-03-03 09:55:00 +0200

    Added ReportsForm.

---
+++ CustomerMaintenanceProject/ReportsForm.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomerMaintenanceSystem
+{
+    using System.Windows.Forms;
+
+    public partial class ReportsForm : Form
+    {
+        public ReportsForm()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
